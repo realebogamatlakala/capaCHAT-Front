17068ebfe749356268eea4eb66d0839c1bccf61e
Realeboga Matlakala <Realeboga.Matlakala@capaciti.org.za>
2024-04-19 16:08:26 +0200

    front-end

---
+++ capa-chat/src/Pages/ChatroomPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatroomPage = () => {
+    return <div>Index Page</div>
+};
+
+export default ChatroomPage;
---
+++ capa-chat/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "./styles/common.css";
+
+ReactDOM.render(<App /> , document.getElementById('root'));
